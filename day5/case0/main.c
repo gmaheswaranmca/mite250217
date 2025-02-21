95e1994009dd09bd3501cdeba45114a9d131d1fe
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-02-21 12:30:21 +0530

    c lang

--- day5/case0/main.c
+++ day5/case0/main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include "Employee.h"
+#include "stack.h"
 void readEmployees(Employee emp[], int n);
 void displayEmployees(Employee emp[], int n);
+void displayEmployeesUsingStack(Employee emp[], int n);
 void manageEmployees();
 int main() {    
     manageEmployees(); 
@@ -15,6 +17,7 @@ void manageEmployees() {
         printf("Choices:\n");
         printf("1-Read Employees\n");
         printf("2-Display Employees\n");
+        printf("3-Display Employees using Stack\n");
         printf("91-Exit\n");
         printf("Your Choice:");
         scanf("%d",&choice);
@@ -28,6 +31,9 @@ void manageEmployees() {
                 displayEmployees(employees, employeeCount);    
             } break;
             case 3: {
+                displayEmployeesUsingStack(employees, employeeCount);    
+            } break;
+            case 91: {
                 printf("Thank you for using app...\n");
             } break;
         }
@@ -47,6 +53,24 @@ void displayEmployees(Employee emp[], int n) {
     for(int I = 0; I < n; I++) {
         printf("|%5d|%-20s|%8.2f|%10s|\n", emp[I].id, emp[I].name, 
                                 emp[I].salary, emp[I].department);
+    }
+    printf("--------------------------------------------------------------------\n");
+}
+
+void displayEmployeesUsingStack(Employee emp[], int n) {
+    Stack stk;
+    StackInit(&stk);
+    for(int I = 0; I < n; I++) {
+        StackPush(&stk, emp[I]);
+    }
+    printf("--------------------------------------------------------------------\n");
+    printf("|%5s|%-20s|%8s|%10s|\n", "ID", "Name", "Salary", "Department");
+    printf("--------------------------------------------------------------------\n");
+    while(!StackEmpty(&stk)) {
+        Employee employee = StackTop(&stk); 
+        printf("|%5d|%-20s|%8.2f|%10s|\n", employee.id, employee.name, 
+            employee.salary, employee.department);
+        StackPop(&stk);
     }
     printf("--------------------------------------------------------------------\n");
 }
